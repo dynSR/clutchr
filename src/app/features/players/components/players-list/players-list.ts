f596cbba5fd1507201af9017773177a7e5ad96b9
dynSR <dylan.schumacker+github@outlook.fr>
2026-04-05 12:11:27 +0200

    Added Player list page place holder and elements

--- src/app/features/players/components/players-list/players-list.ts
+++ src/app/features/players/components/players-list/players-list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PlayerListRow } from './components/player-list-row/player-list-row';
 
 @Component({
   selector: 'app-players-list',
-  imports: [],
+  imports: [PlayerListRow],
   templateUrl: './players-list.html',
   styleUrl: './players-list.scss',
 })
